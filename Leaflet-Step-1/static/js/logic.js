36a1f511d0edd1dc045d561d096d28f1b7325522
Christina Sok <christin11@outlook.com>
2022-05-12 12:42:23 -0400

    logic.js tbc

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,9 @@
+// leaflet-challenge - logic.js
+
+// Earthquakes GeoJSON URL Variable
+var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Initialize & Create Layers
+var layer = new L.LayerGroup();
+
+// Define Tile Layers Variable 
