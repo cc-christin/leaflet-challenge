7179e770cec076fec8cb5a48b4b0210b8dc3aede
Christina Sok <christin11@outlook.com>
2022-05-13 16:37:37 -0400

    more project planning and added default mapbox api key

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY2MtY2hyaXN0aW4iLCJhIjoiY2wzNHdicDVqMjE5MDNibWgxanh3MTU4cSJ9.ntwjmWBDt3Ty-7Tzvfn2mg";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,13 @@
 // leaflet-challenge - logic.js
 
 // Earthquakes GeoJSON URL Variable
-var URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-// Initialize & Create Layers
-var layer = new L.LayerGroup();
+// GET reequest to query URL
+d3.json(queryURL).then(function (data) {
+    createFeatures(data.features);
+});
+
+
 
 // Define Tile Layers Variable 
